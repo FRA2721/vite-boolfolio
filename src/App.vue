31f38989b8a14fbdf0fa43728d345c259b230887
FRA2721 <cimino.francesco36@gmail.com>
2023-01-22 21:49:38 +0100

    Add: header section (debug)

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<!-- script section -->
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+  }
+}
 </script>
+<!-- /script section -->
 
+
+
+<!-- template section -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  <!-- header section -->
+  <header>
+    <AppHeader />
+  </header> 
+  <!-- /header section -->
+
+</template>
+<!-- /template section -->
+
+
+
+<!-- style section -->
+<style lang="scss">
+@use './styles/general.scss' as *;
 </style>
+<!-- /style section -->
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<!-- script section AppHeader -->
+<script>
+export default{
+  name: "AppHeader",
+  data() {
+      return {
+    }
+  }
+}
+</script>
+<!-- /script section AppHeader -->
+
+
+
+<!-- template section AppHeader -->
+<template>
+  <header>
+    header section
+  </header>
+</template>
+<!-- /template section AppHeader -->
+
+
+
+<!-- style section AppHeader -->
+<style lang="scss" scoped>
+</style>
+<!-- /style section AppHEader -->
